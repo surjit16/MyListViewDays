8127e52afeefdb04ac74f94a846543307631e858
Surjit Singh <surjitsingh1618@gmail.com>
2018-04-06 13:53:38 +0530

    Added popUp menu on click list items
    Signed-off-by: Surjit Singh <surjitsingh1618@gmail.com>

--- app/src/main/java/com/example/surjit/mylistview/MainActivity.java
+++ app/src/main/java/com/example/surjit/mylistview/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.surjit.mylistview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -32,6 +34,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(MainActivity.this, ""+parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+                PopupMenu pm = new PopupMenu(MainActivity.this, view);
+                getMenuInflater().inflate(R.menu.my_menu, pm.getMenu());
+                pm.show();
+
+                pm.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast.makeText(MainActivity.this, ""+menuItem.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
             }
         });
     }
